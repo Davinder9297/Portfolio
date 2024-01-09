b8f2edd75f59e9703e54c64594239e36d68dd25b
Davinder9297 <davindergiri765@gmail.com>
2024-01-09 17:16:50 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,310 @@
+export const Bio = {
+  name: "Davinder Kumar",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Davinder9297",
+  resume:
+    "https://drive.google.com/file/d/1fX2-WWQrdP9pl2jVjNzo_C4P83hE33mi/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/davinder--kumar/",
+  twitter: "#",
+  insta: "https://www.instagram.com/davindergiri/",
+  facebook: "https://www.facebook.com/davinder.bawa.16/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "react.svg",
+      },
+      {
+        name: "Redux",
+        image:
+          "/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "next.png",
+      },
+      {
+        name: "HTML",
+        image: "/html.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "/CSS.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "/js.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "/bootstrap.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "/mui.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "/tcss.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "/node.png",
+      },
+      {
+        name: "Express Js",
+        image:
+          "/ex.png",
+      },
+      
+      {
+        name: "MySQL",
+        image:
+          "/mysql.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "/mongodb.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "React Native",
+        image:
+          "/react.svg",
+      },
+      {
+        name: "Expo",
+        image:
+          "/expo.png",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "/android.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "/git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "/github.png",
+      },
+      
+      {
+        name: "Netlify",
+        image:
+          "/netlify.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "/vscode.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "/postman.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "/vercel.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "/cuecoder.png",
+    role: "Fullstack Development Intern",
+    company: "CueCoder Pvt. Ltd.",
+    date: "July 2022 - Sep 2022",
+    desc: "Developed frontend and backend of their blog page with full responsiveness ,Developed Responsive E-commerce website for them which contains all features like category wise products,authentication,add to cart ,payment gateway ,admin panel etc",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "Tailwind CSS",
+      "JavaScript",
+      "React Js",
+      "Express Js",
+      "Node Js",
+      "Next Js",
+      "MongoDB"
+    ],
+    doc: "/offerletter.jpg",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "/ptu.png",
+    school: "I.K.Gujral Punjab Technical University,Mohali",
+    date: "Oct 2020 - Jun 2024",
+    grade: "8.55 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at I.K.Gujral Punjab Technical University,Mohali. I have completed 7 semesters and have a CGPA of 8.55. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a placement coordinator at IKGPTU, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "/mt.png",
+    school: "Govt. Meritorious School,Mohali",
+    date: "Apr 2019 - Apr 2020",
+    grade: "88.2%",
+    desc: "I completed my class 12th high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "PSEB(12th), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "/school.png",
+    school: "Govt.Sen.Sec.School,Machhli Kalan",
+    date: "Apr 2017 - Apr 2018",
+    grade: "76.61%",
+    desc: "I completed my class 10th education at Govt.Sen.Sec.School,Machhli Kalan, where I studied Science with Computer Application.",
+    degree: "PSEB(10th), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Campus Chronicles",
+    date: "Feb 2023 - Mar 2023",
+    description:
+      "It is designed for the reliability of students and teachers.Features includes library management system,club management system,placement management system,events management system,assignment managing system and many more",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Next Js",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "HTML",
+      "CSS",
+      "Tailwind CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/Davinder9297/campus-chronicles",
+    webapp: "https://github.com/Davinder9297/campus-chronicles",
+  },
+  {
+    id: 0,
+    title: "E-commerce website",
+    date: "Sep 2022 - Oct 2022",
+    description:
+      "Features includes category wise products , authentication ,add to cart,payment gateway ,admin panel etc.Fully responsive web app.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "MongoDb", "Node Js","Next Js","Tailwind CSS"],
+    category: "web app",
+    github: "https://github.com/Davinder9297/magento-updated",
+    webapp: "https://github.com/Davinder9297/magento-updated",
+    
+  },
+  {
+    id: 1,
+    title: "Givify",
+    date: "Sep 2023 - Oct 2023",
+    description:
+      "it is donation platform between NGOs and Universities. It includes donation of food , clothes ,books etc. Payment gateway , Chatting feature also there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Tailwind CSS"
+    ],
+    category: "web app",
+    github: "https://github.com/Davinder9297/givify",
+    webapp: "https://givify.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Weather Forecasting App",
+    date: "May 2023 - Jun 2023",
+    description:
+      "It automatically detect user location and show multiple parameter such as temperature,wind speed,humidity etc. It returns all parameters of weather around the world",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["React Native", "Expo", " Open Weather API", "Tailwind CSS"],
+    category: "Android app",
+    github: "https://github.com/Davinder9297",
+    webapp: "https://github.com/Davinder9297",
+    
+  },
+  {
+    id: 3,
+    title: "Waste2Wealth",
+    date: "Nov 2023 - Dec 2023",
+    description:
+      "Your gateway to transforming waste into a valuable resource. At Waste2Wealth, we're on a mission to make a meaningful difference in the world by revolutionizing the way we handle waste materials, with a primary focus on waste plastics.",
+    image:
+      "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+      tags: ["React Js", "MongoDb", "Node Js", "Express Js","Next Js","Tailwind CSS"],
+      category: "web app",
+    github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Instagram Clone",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "Our Instagram clone script has several unique features. Features can be added, deleted, and modified easily in the Instagram clone script.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+      tags: ["React Js", "MongoDb", "Node Js","Tailwind CSS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Job-finder-App",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
